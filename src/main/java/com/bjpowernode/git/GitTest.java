a30259e1f46806f5dbee5c9f32d9f47a4d59bd58
tony <tonyz001@163.com>
2022-01-15 11:21:36 +0800

    second commit

---
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.git;
+
+/**
+ * @author tony
+ * @creat 2022-01-15 11:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
